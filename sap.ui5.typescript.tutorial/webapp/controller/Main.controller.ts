d660bfcb17c482060c43c49b99dcc440517d3f88
Vladimir Vuckovic <vidrak97@gmail.com>
2023-08-21 12:54:15 +0200

    excercise 2 completed

--- sap.ui5.typescript.tutorial/webapp/controller/Main.controller.ts
+++ sap.ui5.typescript.tutorial/webapp/controller/Main.controller.ts
@@ -1,11 +1,27 @@
-import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
+
+type WeatherInfo = {
+	current_weather: {
+		temperature: number,
+		windspeed: number,
+		winddirection: number
+	}
+}
 
 /**
  * @namespace sap.ui5.typescript.tutorial.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	onInit(): void {
+		const model = new JSONModel();
+		this.setModel(model);
+		void this.loadWeatherData();
+	}
+
+	async loadWeatherData(lat = "49.31", lon = "8.64") { // default coordinates: Walldorf
+		const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`);
+		const jsonData = await response.json() as WeatherInfo;
+		(this.getModel() as JSONModel).setData(jsonData);
 	}
 }
